db25e3b76e20fd623458592b042d7685ac04d914
perdub <aleksejspakovskij968@gmail.com>
2022-07-08 22:01:28 +0300

    Добавьте файлы проекта.

---
+++ HLPrint.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HentaiWorld
+{
+    /// <summary>
+    /// Use this class instead console
+    /// </summary>
+    static class HLPrint
+    {
+        static DateTime startupTime;
+        static HLPrint()
+        {
+            startupTime = DateTime.Now;
+        }
+        public static void Print(string s, bool newLine = false)
+        {
+            if (newLine)
+            {
+                Console.WriteLine(s.ToPrint());
+                return;
+            }
+            Console.Write(s.ToPrint());
+        }
+        public static void Print(string s, ConsoleColor color, bool newLine = false)
+        {
+            ConsoleColor last = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            if (newLine)
+            {
+                Console.WriteLine(s.ToPrint());
+                return;
+            }
+            Console.Write(s.ToPrint());
+            Console.ForegroundColor = last;
+        }
+        public static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+        public static string ToPrint(this string s)
+        {
+            return $"[{DateTime.Now - startupTime}|{DateTime.Now.ToString("HH:mm:ss.fff")}]{s}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HentaiWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HLPrint.Print("Hentai World Server", true);
+            HLPrint.ReadLine();
+        }
+    }
+}
